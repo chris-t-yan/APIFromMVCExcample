ae3e8a8f5ed030cac58fa4e194906e3ef54b5448
Hristijan Taseski <hristijan.taseski@gmail.com>
2021-02-17 15:12:34 +0100

    init commit

---
+++ Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using CallingAPIFromMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IUserService
+    {
+        public  Task<List<Student>> GetAllStudents();
+        public Task<Student> GetById(int studentId);
+        public Task<Student> AddUpdateStudent(Student student);
+        public Task<string> Delete(int studentId);
+
+
+
+    }
+}
